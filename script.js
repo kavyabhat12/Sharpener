54998156763f44fd7db01b1cd8efe876ab3c97b9
kavyabhat12 <kavyabhatps@gmail.com>
2024-06-12 11:59:44 +0530

    dommanipulation

---
+++ script.js
@@ -0,0 +1,16 @@
+// Write your code below:
+const mainhead=document.getElementById('main-heading');
+mainhead.textContent='Fruit World';
+mainhead.style.color='orange';
+
+const header=document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom='3px solid orange';
+
+const basket=document.getElementById('basket-heading');
+basket.style.color='green';                                    
+
+
+
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
